1a503b6921a8126aa19aff59a780cc917b557e47
MoEzwawi <mezwawi96@gmail.com>
2023-09-14 23:22:54 +0200

    new random num generator

--- script/index.js
+++ script/index.js
@@ -3,7 +3,7 @@ const createSquares = function (){
     for (let i = 0; i < 90; i++) {
         const square = document.createElement('div')
         square.classList.add('square')
-        square.setAttribute('id',i)
+        square.setAttribute('id',i+1)
         const numberOfTheSquare = document.createElement('p')
         numberOfTheSquare.innerText = i + 1
         square.appendChild(numberOfTheSquare)
@@ -12,9 +12,27 @@ const createSquares = function (){
 }
 createSquares()
 
+const allTheNumbers = []
+for (let i = 0; i < 90; i++) {
+    allTheNumbers.push(i+1)
+}
+
+const dealer = function (){
+    if (allTheNumbers.length === 0) {
+        const msg = confirm('Hai finito le caselle! Vuoi giocare di nuovo? :)')
+        if (msg) {
+            location.reload()
+        }
+    } else {
+        const i = Math.floor(Math.random()*allTheNumbers.length)
+        const randomNumber = allTheNumbers.splice(i,1)[0]
+        return randomNumber
+    }
+}
+
 const button = document.getElementById('btn')
 button.addEventListener('click', function(){
-    const newNumber = Math.floor(Math.random()*75)
-    const targetSquare = document.getElementById(newNumber.toString())
+    let myNum = dealer()
+    const targetSquare = document.getElementById(myNum.toString())
     targetSquare.classList.add('target')
 })
